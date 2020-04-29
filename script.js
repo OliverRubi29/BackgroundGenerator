ae3cab6e7b2c92d01328910f45052fc8b2816a1d
OliveTree <32791829+OwlyOwls@users.noreply.github.com>
2020-04-30 01:05:17 +0800

    Angle Input
    Customizeable angle for gradient now available.

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ var h3 = document.querySelector("h3")
 var color1 = document.querySelector(".color1")
 var color2 = document.querySelector(".color2")
 var body = document.getElementById("gradient")
+var colorAngle = document.querySelector(".angle")
 
-h3.textContent = body.style.background + ";";
+h3.textContent = "linear-gradient(" 
+	+ colorAngle.value
+	+ "deg, "
+	+ color1.value
+	+ ", "
+	+ color2.value
+	+ ")";
 
 function updateColor(){
-	body.style.background = "linear-gradient(to right, " 
+	body.style.background = "linear-gradient(" 
+	+ colorAngle.value
+	+ "deg, "
 	+ color1.value
 	+ ", "
 	+ color2.value
@@ -14,5 +23,27 @@ function updateColor(){
 
 	h3.textContent = body.style.background + ";";
 }
-color1.addEventListener("input", updateColor)
-color2.addEventListener("input", updateColor)
+
+function isInputValid(){
+	if(Number(colorAngle.value) >= -360 && Number(colorAngle.value) <= 360){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function updateAngle(event) {
+	if (colorAngle.value.length > 0 && event.keyCode === 13 ) {
+		
+	console.log(colorAngle.value);
+		if(isInputValid()){
+			updateColor();
+		}else{
+			alert("Invalid Input!");
+		}
+	}
+}
+
+color1.addEventListener("input", updateColor);
+color2.addEventListener("input", updateColor);
+colorAngle.addEventListener("keypress", updateAngle);
